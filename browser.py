0b8c7b2e2e0ffedb661c479f5d6e79eb26f9faac
LuizFelipeOliver <luiz.rya@gmail.com>
2025-03-10 22:28:37 -0300

    feat: add graph to project ✨

--- browser.py
+++ browser.py
@@ -1,73 +1,20 @@
-import socket, ssl
+import tkinter
 
-class URL:
-    def __init__(self,url):
-        self.scheme, url = url.split("://",1);
-        assert self.scheme in ["http", "https"]
+WIDTH, HEIGHT = 800, 600
 
-        if self.scheme == "http":
-            self.port = 80
-        elif self.scheme == "https":
-            self.port = 443
-
-        self.host, url = url.split("/",1);
-        self.path = "/" + url;
-
-        if ":" in self.host:
-            self.host, port = self.host.split(":",1)
-            self.port = int(port)
-
-    def request(self):
-        s = socket.socket(
-            family=socket.AF_INET,
-            type=socket.SOCK_STREAM,
-            proto=socket.IPPROTO_TCP,
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(
+            self.window,
+            width=WIDTH,
+            height=HEIGHT
         )
-        s.connect((self.host, self.port))
+        self.canvas.pack()
 
-        if self.scheme == "https":
-            ctx = ssl.create_default_context()
-            s = ctx.wrap_socket(s, server_hostname=self.host)
-
-        request = "GET {} HTTP/1.0\r\n".format(self.path)
-        request += "Host: {}\r\n".format(self.host)
-        request += "\r\n"
-        s.send(request.encode("utf8"))
-
-        response = s.makefile("r", encoding="utf-8",newline="\r\n")
-
-        statusline = response.readline()
-        version, status, explanation = statusline.split(" ",2)
-
-        response_headers = {}
-        while True:
-            line = response.readline()
-            if line == "\r\n": break
-            header, value = line.split(":",1)
-            response_headers[header.casefold()] = value.strip()
-
-        assert "transfer-encoding" not in response_headers
-        assert "content-encoding" not in response_headers
-
-        content = response.read()
-        s.close()
-
-        return content
-
-def show(body):
-    in_tag = False
-    for c in body:
-        if c == "<":
-            in_tag = True;
-        elif c == ">":
-            in_tag = False
-        elif not in_tag:
-            print(c,end="")
-
-def load(url):
-    body = url.request()
-    show(body)
-
-if __name__ == "__main__":
-    import sys
-    load(URL(sys.argv[1]))
+    def load(url):
+        body = url.request()
+        show(body)
+        self.canvas.create_rectangle(10, 20, 400, 300)
+        self.canvas.create_oval(100, 100, 150, 150)
+        self.canvas.create_text(200, 150, text="Hi!")
